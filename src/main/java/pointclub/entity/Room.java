8638bc5a5386a2f3ef949679c8d4d8754369d5b0
tamirer <tamirer@post.bgu.ac.il>
2022-06-05 15:22:38 +0300

    Add Relations to users_rooms table

--- src/main/java/pointclub/entity/Room.java
+++ src/main/java/pointclub/entity/Room.java
@@ -1,15 +1,43 @@
 package pointclub.entity;
 
-import lombok.Data;
+import lombok.*;
+import org.hibernate.Hibernate;
 
 import javax.persistence.*;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "rooms", schema = "pointclub")
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 public class Room {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
+
+    @ManyToMany
+    @JoinTable(name = "users_rooms",
+            joinColumns = @JoinColumn(name = "room_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id"),
+            schema = "pointclub")
+    @ToString.Exclude
+    private Set<User> users = new LinkedHashSet<>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Room room = (Room) o;
+        return Objects.equals(id, room.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
--- src/main/java/pointclub/entity/User.java
+++ src/main/java/pointclub/entity/User.java
@@ -1,15 +1,43 @@
 package pointclub.entity;
 
-import lombok.Data;
+import lombok.*;
+import org.hibernate.Hibernate;
 
 import javax.persistence.*;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
 
 @Entity
-@Table(name = "users", schema = "public")
-@Data
+@Table(name = "users", schema = "pointclub")
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
+
+    @ManyToMany
+    @JoinTable(name = "users_rooms",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "room_id"),
+            schema = "pointclub")
+    @ToString.Exclude
+    private Set<Room> rooms = new LinkedHashSet<>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
