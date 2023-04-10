5936ff9491a550d80212056736ed72fe4d980b47
Tamir Erez <tamirerez3@gmail.com>
2023-04-10 13:43:17 +0300

    Serialize Message Manually so Room and User are Sent as Ids and not the Entire Objects

--- src/main/java/pointclub/entity/Message.java
+++ src/main/java/pointclub/entity/Message.java
@@ -1,9 +1,11 @@
 package pointclub.entity;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import pointclub.serializer.MessageSerializer;
 
 import javax.persistence.*;
 import java.util.Date;
@@ -14,6 +16,7 @@ import java.util.Date;
 @Setter
 @ToString
 @RequiredArgsConstructor
+@JsonSerialize(using = MessageSerializer.class)
 public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
---
+++ src/main/java/pointclub/serializer/MessageSerializer.java
@@ -0,0 +1,30 @@
+package pointclub.serializer;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import pointclub.entity.Message;
+
+import java.io.IOException;
+
+public class MessageSerializer extends StdSerializer<Message> {
+
+    public MessageSerializer() {
+        this(null);
+    }
+
+    public MessageSerializer(Class<Message> t) {
+        super(t);
+    }
+
+    @Override
+    public void serialize(Message message, JsonGenerator jgen, SerializerProvider serializerProvider) throws IOException {
+        jgen.writeStartObject();
+        jgen.writeNumberField("serverId", message.getServerId());
+        jgen.writeStringField("content", message.getContent());
+        jgen.writeNumberField("senderId", message.getSender().getServerId());
+        jgen.writeNumberField("roomId", message.getRoom().getServerId());
+        jgen.writeNumberField("sendTime", message.getSendTime().getTime());
+        jgen.writeEndObject();
+    }
+}
